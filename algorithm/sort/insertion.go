26f2d46f6225639227ad0c50c35bef67272bcbf5
ghongli <18810027593@163.com>
2023-10-29 18:17:18 +0800

    add insertion sort code

--- algorithm/sort/insertion.go
+++ algorithm/sort/insertion.go
@@ -1,1 +1,16 @@
 package sort
+
+// 将未排序的元素逐个插入到已排序部分的正确位置，直到所有元素都被插入
+func insertionSor(arr []int) {
+	n := len(arr)
+	for i := 1; i < n; i++ {
+		val := arr[i]
+		j := i - 1
+		// > min to max, < max to min
+		for j >= 0 && arr[j] > val {
+			arr[j+1] = arr[j]
+			j = j - 1
+		}
+		arr[j+1] = val
+	}
+}
--- algorithm/sort/insertion_test.go
+++ algorithm/sort/insertion_test.go
@@ -1,1 +1,11 @@
 package sort
+
+import (
+	"testing"
+)
+
+func TestInsertionSort(t *testing.T) {
+	arr := []int{64, 25, 12, 23, 11}
+	insertionSor(arr)
+	t.Log(arr)
+}
