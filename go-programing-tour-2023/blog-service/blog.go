d28c279a1aba6d4036b74be042ea6cea51010edb
ghongli <18810027593@163.com>
2023-11-01 19:01:33 +0800

    add md5 encode kit

---
+++ go-programing-tour-2023/blog-service/blog.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+
+}
+
+var (
+	port      string
+	runMode   string
+	config    string
+	isVersion bool
+)
---
+++ go-programing-tour-2023/blog-service/pkg/kit/md5.go
@@ -0,0 +1,13 @@
+package kit
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+func EncodeMD5(val string) string {
+	m := md5.New()
+	m.Write([]byte(val))
+
+	return hex.EncodeToString(m.Sum(nil))
+}
