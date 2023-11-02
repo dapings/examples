9ad6db6991c9981e69d438881f77c400d50fb3b2
ghongli <18810027593@163.com>
2023-11-02 20:38:01 +0800

    blog service db conn

--- go-programing-tour-2023/blog-service/blog.go
+++ go-programing-tour-2023/blog-service/blog.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/dapings/examples/go-programing-tour-2023/blog-service/global"
+	"github.com/dapings/examples/go-programing-tour-2023/blog-service/internal/model"
 	"github.com/dapings/examples/go-programing-tour-2023/blog-service/internal/routers"
 	"github.com/dapings/examples/go-programing-tour-2023/blog-service/pkg/setting"
 	"github.com/gin-gonic/gin"
@@ -28,6 +29,11 @@ func init() {
 	err := setupSetting()
 	if err != nil {
 		log.Fatalf("init.setupSetting err: %v", err)
+	}
+
+	err = setupDBEngine()
+	if err != nil {
+		log.Fatalf("init.setupDBEngine err: %v", err)
 	}
 }
 
@@ -59,5 +65,16 @@ func setupSetting() error {
 
 	global.ServerSetting.ReadTimeout *= time.Second
 	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
+}
+
+func setupDBEngine() error {
+	// 注：:= 会重新声明并创建左侧的新局部变量，因此在其他包调用变量时，仍然是 nil，达不到可用标准
+	// 在赋值时并没有赋值到真正需要赋值的变量上
+	var err error
+	global.DBEngine, err = model.NewDBEngine(global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- go-programing-tour-2023/blog-service/internal/model/model.go
+++ go-programing-tour-2023/blog-service/internal/model/model.go
@@ -1,5 +1,14 @@
 package model
 
+import (
+	"fmt"
+
+	"github.com/dapings/examples/go-programing-tour-2023/blog-service/global"
+	"github.com/dapings/examples/go-programing-tour-2023/blog-service/pkg/setting"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
 type Model struct {
 	ID         uint32 `gorm:"primary_key" json:"id"`
 	CreatedBy  string `json:"created_by"`
@@ -8,4 +17,27 @@ type Model struct {
 	ModifiedOn uint32 `json:"modified_on"`
 	DeletedOn  uint32 `json:"deleted_on"`
 	IsDel      uint8  `json:"is_deleted"`
+}
+
+func NewDBEngine(dbSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
+	s := "%s:%s@tcp(%s)/%s?charset=%s&parseTime=%t&loc=Local"
+	db, err := gorm.Open(dbSetting.DBType, fmt.Sprintf(s,
+		dbSetting.UserName,
+		dbSetting.Password,
+		dbSetting.Host,
+		dbSetting.DBName,
+		dbSetting.Charset,
+		dbSetting.ParseTime,
+	))
+	if err != nil {
+		return nil, err
+	}
+
+	if global.ServerSetting.RunMode == "debug" {
+		db.LogMode(true)
+	}
+	db.SingularTable(true)
+	db.DB().SetMaxIdleConns(dbSetting.MaxIdleConns)
+	db.DB().SetMaxOpenConns(dbSetting.MaxOpenConns)
+	return db, nil
 }
