0146b06812716f29ab1c59f2205697df8733847a
ghongli <18810027593@163.com>
2023-11-02 11:32:14 +0800

    use gin run a http demo, by Default run /ping route

--- go-programing-tour-2023/blog-service/blog.go
+++ go-programing-tour-2023/blog-service/blog.go
@@ -1,7 +1,17 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSONP(http.StatusOK, gin.H{"message": "pong"})
+	})
+	_ = r.Run()
 }
 
 var (
