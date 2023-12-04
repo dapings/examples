70ce24539614b0efd396e132e4c884ec4e61f2dd
ghongli <18810027593@163.com>
2023-12-04 23:10:16 +0800

    protoc plugin public generator logic

---
+++ go-programing-tour-2023/protoc-gen-go-tour/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+	"os"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/protoc-gen-go/generator"
+)
+
+func main() {
+	// 写入固定的公共generator逻辑
+	g := generator.New()
+
+	data, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		g.Error(err, "reading input")
+	}
+
+	if err := proto.Unmarshal(data, g.Request); err != nil {
+		g.Error(err, "parsing input proto")
+	}
+}
