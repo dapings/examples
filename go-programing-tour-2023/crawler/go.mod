da43c8bba459c6abe5030c315fe66c82e292cc9b
ghongli <18810027593@163.com>
2023-12-07 22:44:54 +0800

    crawler encoding html text by utf8

--- go-programing-tour-2023/crawler/go.mod
+++ go-programing-tour-2023/crawler/go.mod
@@ -1,3 +1,8 @@
 module github.com/dapings/examples/go-programing-tour-2023/crawler
 
 go 1.20
+
+require (
+	golang.org/x/net v0.19.0
+	golang.org/x/text v0.14.0
+)
--- go-programing-tour-2023/crawler/main.go
+++ go-programing-tour-2023/crawler/main.go
@@ -1,19 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"io"
 	"log"
 	"net/http"
 	"strings"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 )
 
 func main() {
-	url := "https://www.thepaper.cn/"
+	// url := "https://www.thepaper.cn/"
+	url := "https://www.chinanews.com.cn/"
+	body, err := Fetch(url)
+	if err != nil {
+		log.Printf("read content failed: %v", err)
+		return
+	}
+
+	handleLinks(body)
+}
+
+func Fetch(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Printf("fetch url(%s) error: %v", url, err)
-		return
+		panic(err)
 	}
 
 	defer func(closer io.ReadCloser) {
@@ -27,9 +44,29 @@ func main() {
 		log.Printf("error status code: %v", resp.StatusCode)
 	}
 
-	body, err := io.ReadAll(resp.Body)
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DetermineEncoding(bodyReader)
+	// 将HTML文本从特定编码转换为utf8编码
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return io.ReadAll(utf8Reader)
+}
+
+// DetermineEncoding 检测并返回当前HTML文本的编码格式
+func DetermineEncoding(r *bufio.Reader) encoding.Encoding {
+	// 如果返回的文本小于1024个字节，则说明文本有问题，直接使用utf8编码
+	peek, err := r.Peek(1024)
 	if err != nil {
-		log.Printf("fail to read content: %v", err)
+		log.Printf("determin encoding err: %v", err)
+		return unicode.UTF8
+	}
+
+	// 检测并返回当前HTML文本的编码格式
+	e, _, _ := charset.DetermineEncoding(peek, "")
+	return e
+}
+
+func handleLinks(body []byte) {
+	if len(body) == 0 {
 		return
 	}
 
