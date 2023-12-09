1e599d6acc7006cf4e56ba90a2095b0c6b4d174e
ghongli <18810027593@163.com>
2023-12-09 23:49:26 +0800

    crawler fetch use round robin proxy func

--- go-programing-tour-2023/crawler/main.go
+++ go-programing-tour-2023/crawler/main.go
@@ -6,16 +6,23 @@ import (
 
 	"github.com/dapings/examples/go-programing-tour-2023/crawler/collect"
 	"github.com/dapings/examples/go-programing-tour-2023/crawler/parse"
+	"github.com/dapings/examples/go-programing-tour-2023/crawler/proxy"
 )
 
 func main() {
+	proxyURLs := []string{"http://127.0.0.1:8888", "http://127.0.0.1:8888"}
+	proxyFunc, err := proxy.RoundRobinProxySwitcher(proxyURLs...)
+	if err != nil {
+		log.Printf("round robin proxy switcher failed: %v\n", err)
+		return
+	}
 	url := "https://www.thepaper.cn/"
 	// url := "https://www.chinanews.com.cn/"
 	// url := "https://google.com.hk"
 
 	// douban timeout
 	// url := "https://book.douban.com/subject/1007305/"
-	var f collect.Fetcher = collect.BrowserFetch{Timeout: 300 * time.Millisecond}
+	var f collect.Fetcher = collect.BrowserFetch{Timeout: 300 * time.Millisecond, Proxy: proxyFunc}
 	body, err := f.Get(url)
 	if err != nil {
 		log.Printf("read content failed: %v", err)
