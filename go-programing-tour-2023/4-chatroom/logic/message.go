dd0111546ed7a14f41ac30815f76b27f92297b67
ghongli <18810027593@163.com>
2024-01-01 23:37:53 +0800

    chatroom user message type and some funcs

--- go-programing-tour-2023/4-chatroom/logic/message.go
+++ go-programing-tour-2023/4-chatroom/logic/message.go
@@ -2,6 +2,16 @@ package logic
 
 import (
 	"time"
+
+	"github.com/spf13/cast"
+)
+
+const (
+	MsgTypeNormal    = iota // 普通用户消息
+	MsgTypeWelcome          // 当前用户欢迎消息
+	MsgTypeUserEnter        // 用户进入
+	MsgTypeUserLeave        // 用户退出
+	MsgTypeError            // 错误消息
 )
 
 // Message 给用户发送的消息。
@@ -19,4 +29,55 @@ type Message struct {
 
 	// 用户列表不通过 websocket 下发
 	// Users []*User `json:"users"`
+}
+
+func NewMessage(user *User, content, clientTime string) *Message {
+	message := &Message{
+		User:    user,
+		Type:    MsgTypeNormal,
+		Content: content,
+		MsgTime: time.Now().Local(),
+	}
+
+	if clientTime != "" {
+		message.ClientSentTime = time.Unix(0, cast.ToInt64(clientTime))
+	}
+
+	return message
+}
+
+func NewWelcomeMessage(user *User) *Message {
+	return &Message{
+		User:    user,
+		Type:    MsgTypeWelcome,
+		Content: user.NickName + " 您好，欢迎加入聊天室！",
+		MsgTime: time.Now().Local(),
+	}
+}
+
+func NewUserEnterMessage(user *User) *Message {
+	return &Message{
+		User:    user,
+		Type:    MsgTypeUserEnter,
+		Content: user.NickName + " 加入了聊天室",
+		MsgTime: time.Now().Local(),
+	}
+}
+
+func NewUserLeaveMessage(user *User) *Message {
+	return &Message{
+		User:    user,
+		Type:    MsgTypeUserLeave,
+		Content: user.NickName + " 离开了聊天室",
+		MsgTime: time.Now().Local(),
+	}
+}
+
+func NewErrorMessage(content string) *Message {
+	return &Message{
+		User:    System,
+		Type:    MsgTypeError,
+		Content: content,
+		MsgTime: time.Now().Local(),
+	}
 }
