9c623b435e719f074ea6114cdd86a0c6dbfc371e
ghongli <18810027593@163.com>
2024-01-05 23:02:57 +0800

    chatroom websocket handle

---
+++ go-programing-tour-2023/4-chatroom/server/websocket.go
@@ -0,0 +1,32 @@
+package server
+
+import (
+	"log"
+	"net/http"
+
+	"nhooyr.io/websocket"
+)
+
+func WebSocketHandleFunc(w http.ResponseWriter, req *http.Request) {
+	// Accept 从客户端接受 Websocket 握手，并将连接升级到 Websocket。
+	// 如果Origin域与主机不同，Accept将拒绝握手，除非设置了InsecureSkipVerify选项(通过第三个参数AcceptOptions设置)。
+	// 换句话说，默认情况下，它不允许跨源请求。如果发生错误，Accept将始终写入适当的响应。
+	conn, err := websocket.Accept(w, req, &websocket.AcceptOptions{InsecureSkipVerify: true})
+	if err != nil {
+		log.Println("websocket accept error:", err)
+
+		return
+	}
+
+	_ = conn.Close(websocket.StatusUnsupportedData, "")
+
+	// 1. 新用户进来，构建此用户的实例
+	// 2. 开启给用户发送消息的G
+	// 3. 给当前用户发送欢迎信息
+	// 避免token泄漏
+	// 给所有用户告知新用户到来
+	// 4. 将此用户加入广播器的用户列表中
+	// 5. 接收用户消息
+	// 6. 用户离开
+	// 根据读取时的错误，执行不同的Close
+}
