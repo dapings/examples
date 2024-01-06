359af4213e733c89e2310c8534e304a0230aaf4a
ghongli <18810027593@163.com>
2024-01-06 23:52:57 +0800

    chatroom websocket handle new user

--- go-programing-tour-2023/4-chatroom/server/websocket.go
+++ go-programing-tour-2023/4-chatroom/server/websocket.go
@@ -4,7 +4,9 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/dapings/examples/go-programing-tour-2023/chatroom/logic"
 	"nhooyr.io/websocket"
+	"nhooyr.io/websocket/wsjson"
 )
 
 func WebSocketHandleFunc(w http.ResponseWriter, req *http.Request) {
@@ -21,7 +23,27 @@ func WebSocketHandleFunc(w http.ResponseWriter, req *http.Request) {
 	_ = conn.Close(websocket.StatusUnsupportedData, "")
 
 	// 1. 新用户进来，构建此用户的实例
+	token := req.FormValue("token")
+	nickname := req.FormValue("nickname")
+	if l := len(nickname); l < 2 || l > 20 {
+		log.Println("nickname illegal: ", nickname)
+		_ = wsjson.Write(req.Context(), conn, logic.NewErrorMessage("非法昵称，昵称长度：2-20"))
+		_ = conn.Close(websocket.StatusUnsupportedData, "nickname illegal!")
+		return
+	}
+
+	if !logic.Broadcaster.CanEnterRoom(nickname) {
+		log.Println("nickname exists： ", nickname)
+		_ = wsjson.Write(req.Context(), conn, logic.NewErrorMessage("昵称已存在！"))
+		_ = conn.Close(websocket.StatusUnsupportedData, "nickname exists!")
+		return
+	}
+
+	userHasToken := logic.NewUser(conn, token, nickname, req.RemoteAddr)
+
 	// 2. 开启给用户发送消息的G
+	go userHasToken.SendMessage(req.Context())
+
 	// 3. 给当前用户发送欢迎信息
 	// 避免token泄漏
 	// 给所有用户告知新用户到来
